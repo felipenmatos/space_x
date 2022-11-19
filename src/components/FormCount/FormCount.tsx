be2036a427e6e57481b3f723404c6c42c1158e9c
felipenmatos <notfelipe2@hotmail.com>
2022-11-19 18:20:45 -0300

    style: count component responsiveness

--- src/components/FormCount/FormCount.tsx
+++ src/components/FormCount/FormCount.tsx
@@ -27,6 +27,11 @@ const Container = styled.div`
   background: rgba(189, 0, 255, 0.43);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 40px;
+
+  @media (max-width: 888px) {
+    width: 370px;
+    height: 370px;
+  }
 `;
 
 const Icon = styled.img`
